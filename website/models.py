75236997c2dda2d8d87a55faadb03cfe883fcd0e
Quade Howald <quade.howald@gmail.com>
2020-10-20 14:32:44 -0400

    admin page stuff

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Experience(models.Model)
+	# Images
+	image = models.ImageField(upload_to='images/')
+	# Summary
+	summary = models.CharFeild(max_length=200)
