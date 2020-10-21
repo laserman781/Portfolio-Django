19c7521d74100e3ff4df4aa7624dd88cdea05ceb
Quade Howald <quade.howald@gmail.com>
2020-10-21 13:30:31 -0400

    changed colors in style.css

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ class Experience(models.Model):
 	# Images
 	title = models.CharField(max_length=200)
 	date = models.CharField(max_length=200)
-	image = models.ImageField(upload_to='images/', null=True)
+	#image = models.ImageField(upload_to='/images/', null=True)
 	# Summary
 	summary = models.CharField(max_length=200)
 
@@ -13,13 +13,13 @@ class Awards(models.Model):
 	#Title
 	titledate = models.CharField(max_length=200)
 	#description
-	image = models.ImageField(upload_to='images/', null=True)
+	#image = models.ImageField(upload_to='images/', null=True)
 	description = models.CharField(max_length=200)
 
 class Leadership(models.Model):
 	#Title
 	titledate = models.CharField(max_length=200)
-	image = models.ImageField(upload_to='images/', null=True)
+	#image = models.ImageField(upload_to='images/', null=True)
 	#description
 	description = models.CharField(max_length=200)
 
